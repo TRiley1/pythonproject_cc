9b6025da033a72aa2fcf9f93e935b732dd708bd0
Thomas Riley <thomas@Thomass-MacBook-Pro.local>
2023-03-24 14:48:13 +0000

    first initial tests run

--- run_test.py
+++ run_test.py
@@ -2,7 +2,7 @@ import unittest
 from tests.adventurer_test import TestAdventurer
 from tests.item_test import TestItem
 from tests.loot_test import TestLoot
-from tests.store_test import TestStore
+# from tests.store_test import TestStore
 
 if __name__ == '__main__':
     unittest.main()
--- tests/item_test.py
+++ tests/item_test.py
@@ -5,3 +5,9 @@ class TestItem(unittest.TestCase):
 
     def setUp(self):
         self.item = Item("Sword", "The Sword of Gods", "Super Rare", 100000)
+
+    def test_item_has_name(self):
+        self.assertEqual("The Sword of Gods", self.item.name)
+
+    def test_item_has_value(self):
+        self.assertEqual(100000, self.item.value)
