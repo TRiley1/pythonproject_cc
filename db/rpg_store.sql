d39543ce2630e13d0df2825ffd218a92fbddff06
Thomas Riley <thomas@Thomass-MacBook-Pro.local>
2023-03-25 10:24:12 +0000

    added a many to many relationship between items and adventurers

--- db/rpg_store.sql
+++ db/rpg_store.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS item_adv;
 DROP TABLE IF EXISTS adventurers;
 DROP TABLE IF EXISTS items;
 
@@ -11,9 +12,23 @@ CREATE TABLE items (
 
 CREATE TABLE adventurers (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    loot_id INT NOT NULL REFERENCES items(id)
+    name VARCHAR(255)
+);
+
+CREATE TABLE item_adv (
+    id SERIAL PRIMARY KEY,
+    adventurer_id INT NOT NULL REFERENCES adventurers(id) ON DELETE CASCADE,
+    loot_id INT NOT NULL REFERENCES items(id) ON DELETE CASCADE,
+    review TEXT
 );
 
 INSERT INTO items (type, name, rarity, value) VALUES ('helmet', 'rusty helmet', NULL, NULL);
-INSERT INTO adventurers (name, loot_id) VALUES ('Thorfin', 1);
+INSERT INTO items (type, name, rarity, value) VALUES ('sword', 'dirty dagger', NULL, NULL);
+INSERT INTO adventurers (name) VALUES ('Thorfin');
+INSERT INTO adventurers (name) VALUES ('Thorkell');
+INSERT INTO adventurers (name) VALUES ('Thor');
+INSERT INTO item_adv(adventurer_id, loot_id) VALUES (1,1);
+INSERT INTO item_adv(adventurer_id, loot_id) VALUES (2,1);
+INSERT INTO item_adv(adventurer_id, loot_id) VALUES (1,2);
+
+-- SELECT adventurers.* FROM adventurers INNER JOIN item_adv ON item_adv.adventurer_id = adventurers.id WHERE item_adv.loot_id = 1
