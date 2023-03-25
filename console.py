ecd62357da18ed76897e454314c5a68a515b7747
Thomas Riley <thomas@Thomass-MacBook-Pro.local>
2023-03-25 16:32:24 +0000

    added an update to items and find_loot function to adventurers

--- console.py
+++ console.py
@@ -4,10 +4,15 @@ import repositories.item_repo as item_repo
 # try and add an item to the database
 
 item1 = Item("Misc", "Rusty Spoon")  
-item1 = Item('boo', 'The Ladel of Gods', 'Ultra Rare', 1000)
+# item1 = Item('boo', 'The Ladel of Gods', 'Ultra Rare', 1000)
 # item_repo.item_update(item1)
 item_repo.save(item1)
 
+item1.name = 'The Ladel of the Gods'
+item1.rarity = 'Ultra Rare'
+item1.value = 1000
+
+item_repo.item_update(item1)
 
 results = item_repo.select_all()
 
--- models/adventurer.py
+++ models/adventurer.py
@@ -1,5 +1,9 @@
 class Adventurer:
-    def __init__(self, name, loot, id = None):
+    def __init__(self, name,loot, id = None):
         self.name = name 
         self.loot = loot 
         self.id = id
+        self.inventory = []
+
+    def find_loot(self,item):
+        self.inventory.append(item)
