351998c3205a9e2a820c51eb5e8413a648f950b0
Thomas Riley <thomas@Thomass-MacBook-Pro.local>
2023-03-25 16:51:21 +0000

    put some ideas together for random feel

--- models/adventurer.py
+++ models/adventurer.py
@@ -3,6 +3,7 @@ class Adventurer:
         self.name = name 
         self.id = id
         self.inventory = []
-
+        
+    # item being an object
     def find_loot(self,item):
         self.inventory.append(item)
--- models/store.py
+++ models/store.py
@@ -4,3 +4,17 @@ class Store:
         self.money = money 
         self.stock = {} 
         self.appraisel = {}
+
+    # takes in an item and adds a random new item.name, item.rarity and item.value
+    # appraisel is a dict of three keys: name, rarity and item te values are a list of names, rarities and values 
+    # {name: [list of names],
+    #  rarities: [list of rarities],
+    # values: [list of values]}
+
+    def appraisel(item, appraisel,randomNum):
+        item.name = appraisel['name'][randomNum]
+        item.rarity = appraisel['rarity'][randomNum]
+        item.value = appraisel['value'][randomNum]
+
+    # I think this could also be done with an extra table in the database filled with name, rarity and value and assiging.
+    # doing an SQL random query to return a random object. 
