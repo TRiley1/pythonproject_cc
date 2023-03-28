56a3519c63f797347f66bbc20aad45ac1451803f
Thomas Riley <thomas@Thomass-MacBook-Pro.local>
2023-03-28 14:01:07 +0100

    added CRUD for adventurers

--- controllers/adventurer_controller.py
+++ controllers/adventurer_controller.py
@@ -4,12 +4,29 @@ import repositories.adventurer_repo as adventurer_repo
 
 adventurer_blueprint = Blueprint("adventurer", __name__)
 
+@adventurer_blueprint.route('/adventurer/new')
+def adventurer():
+    return render_template('/adventurers/new.html')
+
+@adventurer_blueprint.route('/adventurer/new', methods = ['POST'])
+def new_adventurer():
+    name = request.form['name']
+    adventurer = Adventurer(name)
+    adventurer_repo.save(adventurer)
+    return redirect('/')
+
+@adventurer_blueprint.route('/adventurer/<user_id>/edit')
+def edit_adventurer(user_id):
+    adventurer = adventurer_repo.select(user_id)
+    return render_template('adventurers/edit.html', user_id = user_id, adventurer= adventurer)
+
+
 @adventurer_blueprint.route('/adventurer/<user_id>/delete', methods = ['POST'])
 def delete_adventurer(user_id):
     adventurer_repo.delete(user_id)
     return redirect('/')
 
-@adventurer_blueprint.route('/adventurer/<user_id>/update', methods = ['POST'])
+@adventurer_blueprint.route('/adventurer/<user_id>/edit', methods = ['POST'])
 def update_adventurer(user_id):
     name = request.form['name']
     adventurer = Adventurer(name,user_id)
